e720f36dfdc9e2d28a916474e90c9d2fb2c24736
R1fty0 <113545024+R1fty0@users.noreply.github.com>
2024-04-04 17:19:47 -0700

    Missile is still broken, have no idea how to fix it.

--- Rockets/scripts/missle.gd
+++ Rockets/scripts/missle.gd
@@ -1,23 +1,25 @@
 extends CharacterBody2D
 
-@export var missle_speed = 100.0
+@export var speed = 100.0
+@export var drag_factor = 2
 @onready var player = $"../Player"
 
-# To Do: Get Missle to face player 
-func _physics_process(_delta):
-	if player == null:
-		return 
-	else:
-		_look_at_player()
-		_movement()
+var current_velocity = Vector2.ZERO
 
-func _movement():
-	pass
+func _ready():
+	current_velocity = speed * Vector2.RIGHT.rotated(rotation).normalized()
 	
-func _look_at_player():
-	# Get player position
-	var player_pos = player.position
-	# Get direction to player
-	var angle_to_player = atan2(player_pos.y - position.y, player_pos.x - position.x)
-	# Rotate missile to face player
-	rotation = angle_to_player
+
+func _physics_process(delta):
+	var direction := Vector2.RIGHT.rotated(rotation).normalized()
+	
+	if player != null:
+		direction = global_position.direction_to(player.global_position)
+		
+	var desired_velocity = direction * speed
+	var previous_velocity = current_velocity
+	var change = (desired_velocity - current_velocity) * drag_factor
+	current_velocity += change
+	velocity = current_velocity
+	look_at(global_position + current_velocity)
+	move_and_slide()
